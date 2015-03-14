1909dcfd9688b41655abd131b173562d24e26c01
eviltobz <eviltobz@hotmail.com>
2015-03-14 22:29:55 +0000

    Adding Team & Riders scaffolds

---
+++ MGAsite/Controllers/RidersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MGAsite.Models;
+
+namespace MGAsite.Controllers
+{
+    public class RidersController : Controller
+    {
+        private MgaEntities db = new MgaEntities();
+
+        // GET: Riders
+        public ActionResult Index()
+        {
+            return View(db.Riders.ToList());
+        }
+
+        // GET: Riders/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rider rider = db.Riders.Find(id);
+            if (rider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rider);
+        }
+
+        // GET: Riders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Riders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RidersID,FullName,DOB")] Rider rider)
+        {
+            if (ModelState.IsValid)
+            {
+                rider.RidersID = Guid.NewGuid();
+                db.Riders.Add(rider);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rider);
+        }
+
+        // GET: Riders/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rider rider = db.Riders.Find(id);
+            if (rider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rider);
+        }
+
+        // POST: Riders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RidersID,FullName,DOB")] Rider rider)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rider).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rider);
+        }
+
+        // GET: Riders/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rider rider = db.Riders.Find(id);
+            if (rider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rider);
+        }
+
+        // POST: Riders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Rider rider = db.Riders.Find(id);
+            db.Riders.Remove(rider);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MGAsite/Controllers/TeamsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MGAsite.Models;
+
+namespace MGAsite.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private MgaEntities db = new MgaEntities();
+
+        // GET: Teams
+        public ActionResult Index()
+        {
+            return View(db.Teams.ToList());
+        }
+
+        // GET: Teams/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        // GET: Teams/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teams/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TeamID,TeamName")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                team.TeamID = Guid.NewGuid();
+                db.Teams.Add(team);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(team);
+        }
+
+        // GET: Teams/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        // POST: Teams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TeamID,TeamName")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(team).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(team);
+        }
+
+        // GET: Teams/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        // POST: Teams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Team team = db.Teams.Find(id);
+            db.Teams.Remove(team);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
